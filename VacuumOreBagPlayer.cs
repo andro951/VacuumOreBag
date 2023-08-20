39eb2c351fd4b4bbe1307c16c59a7ed32b437b6c
andro951 <andro951@gmail.com>
2023-08-19 20:01:38 -0400

    Vacuum Ore Bag 1.0.0.2(19AUG23)     -You will now start the game with an Ore Bag.  (As intended)

---
+++ VacuumOreBagPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using VacuumOreBag.Items;
+
+namespace VacuumOreBag
+{
+	public class VacuumOreBagPlayer : ModPlayer
+	{
+		public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath) {
+			List<Item> items = new List<Item>() {
+				new(ModContent.ItemType<OreBag>())
+			};
+
+			return items;
+		}
+	}
+}
